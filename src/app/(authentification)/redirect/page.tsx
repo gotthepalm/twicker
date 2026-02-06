134e93105f3e0c6274b7b34cd540ea555e1272c7
gotthepalm <denisukvemtil@gmail.com>
2026-02-06 21:23:42 +0200

    edited user and registration page

--- src/app/(authentification)/redirect/page.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
-
-export default async function Redirect() {
-	const session = await auth()
-	if (!session) redirect('/login')
-	if (!session.user.nickname) redirect('/registration')
-	redirect('/')
-}
--- src/app/(profile)/[user]/page.tsx
+++ src/app/(profile)/[user]/page.tsx
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
 import Profile from '@/src/components/Profile';
+import { auth } from '@/auth';
+import AdminProfile from '@/src/components/AdminProfile';
 
 export default async function User({ params }: { params: Promise<{ user: string }> }) {
 	const { user } = await params;
@@ -11,10 +13,18 @@ export default async function User({ params }: { params: Promise<{ user: string 
 			<div>error</div>
 		);
 	}
-
+	const session = await auth()
+	if (dbUser.id !== session?.user.id) {
+		return (
+			<div className='flex gap-2'>
+				<Profile user={dbUser}/>
+			</div>
+		);
+	}
 	return (
 		<div className='flex gap-2'>
-			<Profile user={dbUser}/>
+			<AdminProfile user={dbUser}/>
 		</div>
 	);
+
 }
